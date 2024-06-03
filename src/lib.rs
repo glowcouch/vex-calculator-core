01a2a21b92de19d28a57bb8a6315bb94ff8aedc4
glowcouch <42631203+glowcouch@users.noreply.github.com>
2024-06-04 11:57:16 +1200

    feat: create spacing calculator

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,60 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use spacers::{VexSpacer, VexSpacerSolution};
+use uom::si::f32::*;
+use uom::si::length::inch;
+
+pub mod spacers;
+
+pub struct VexLength {
+    pub length: Length,
+}
+
+impl VexLength {
+    pub fn calculate_spacers(&self) -> Vec<VexSpacerSolution> {
+        let mut solutions: Vec<VexSpacerSolution> = vec![];
+        let spacers = spacers::VexSpacer::get_spacers();
+
+        fn recurse(
+            length: &Length,
+            spacers: &Vec<VexSpacer>,
+            current_soultion: VexSpacerSolution,
+            solutions: &mut Vec<VexSpacerSolution>,
+        ) {
+            let mut spacers_length: Length = Length::default();
+
+            for spacer in current_soultion.spacers.clone() {
+                spacers_length += spacer.thickness;
+            }
+
+            if &spacers_length == length {
+                let mut current_soultion = current_soultion.clone();
+                current_soultion.perfect = true;
+                solutions.push(current_soultion.clone());
+            } else if &spacers_length > length {
+                let mut current_soultion = current_soultion.clone();
+                current_soultion.perfect = false;
+                solutions.push(current_soultion.clone());
+                current_soultion
+                    .spacers
+                    .remove(current_soultion.spacers.len() - 1);
+                solutions.push(current_soultion);
+            } else {
+                for spacer in spacers {
+                    let mut current_soultion = current_soultion.clone();
+                    current_soultion.spacers.push(spacer.clone());
+                    recurse(length, spacers, current_soultion.clone(), solutions);
+                }
+            }
+        }
+
+        recurse(
+            &self.length,
+            &spacers,
+            VexSpacerSolution::default(),
+            &mut solutions,
+        );
+
+        solutions
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +63,16 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = VexLength {
+            length: Length::new::<inch>(0.25),
+        }
+        .calculate_spacers();
+        assert!(result.contains(&VexSpacerSolution {
+            spacers: vec![VexSpacer {
+                thickness: Length::new::<inch>(0.25),
+                od: Length::new::<inch>(3.0 / 8.0),
+            },],
+            perfect: true,
+        }));
     }
 }
---
+++ src/spacers.rs
@@ -0,0 +1,101 @@
+use std::fmt;
+use uom::si::f32::*;
+use uom::si::length::{inch, millimeter};
+
+#[derive(Clone, Copy, PartialEq)]
+pub struct VexSpacer {
+    pub thickness: Length,
+    pub od: Length,
+}
+
+impl fmt::Display for VexSpacer {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "(thickness: {} mm, od: {} mm)",
+            self.thickness.value * 1000.0,
+            self.od.value * 1000.0
+        )
+    }
+}
+
+impl VexSpacer {
+    pub fn get_spacers() -> Vec<VexSpacer> {
+        vec![
+            // 8mm plastic spacers
+            VexSpacer {
+                thickness: Length::new::<millimeter>(8.0),
+                od: Length::new::<inch>(0.32),
+            },
+            // 3/8" OD nylon spacers
+            VexSpacer {
+                thickness: Length::new::<inch>(0.125),
+                od: Length::new::<inch>(3.0 / 8.0),
+            },
+            VexSpacer {
+                thickness: Length::new::<inch>(0.25),
+                od: Length::new::<inch>(3.0 / 8.0),
+            },
+            VexSpacer {
+                thickness: Length::new::<inch>(0.375),
+                od: Length::new::<inch>(3.0 / 8.0),
+            },
+            VexSpacer {
+                thickness: Length::new::<inch>(0.5),
+                od: Length::new::<inch>(3.0 / 8.0),
+            },
+            // 1/2" OD nylon spacers
+            /*VexSpacer {
+                thickness: Length::new::<inch>(0.125),
+                od: Length::new::<inch>(1.0/2.0),
+            },
+            VexSpacer {
+                thickness: Length::new::<inch>(0.25),
+                od: Length::new::<inch>(1.0/2.0),
+            },
+            VexSpacer {
+                thickness: Length::new::<inch>(0.375),
+                od: Length::new::<inch>(1.0/2.0),
+            },
+            VexSpacer {
+                thickness: Length::new::<inch>(0.5),
+                od: Length::new::<inch>(1.0/2.0),
+            },*/
+            // Teflon washer
+            VexSpacer {
+                thickness: Length::new::<inch>(0.04),
+                od: Length::new::<inch>(0.375),
+            },
+            // Steel washer
+            VexSpacer {
+                thickness: Length::new::<inch>(0.032),
+                od: Length::new::<inch>(0.375),
+            },
+        ]
+    }
+}
+
+#[derive(Clone, PartialEq, Default)]
+pub struct VexSpacerSolution {
+    pub spacers: Vec<VexSpacer>,
+    pub perfect: bool,
+}
+
+impl VexSpacerSolution {
+    fn get_thickness(&self) -> Length {
+        let mut thickness: Length = Length::default();
+        for spacer in &self.spacers {
+            thickness += spacer.thickness
+        }
+        thickness
+    }
+    fn get_od(&self) -> Length {
+        let mut od: Length = Length::default();
+        for spacer in &self.spacers {
+            if spacer.od > od {
+                od = spacer.od;
+            }
+        }
+        od
+    }
+}
